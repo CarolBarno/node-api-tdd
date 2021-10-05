b4aa5bc80e1f59d389cf5a3888844e552f1cd3da
Carol-Barno <cbarno@innovexsolutions.co.ke>
2021-10-05 07:58:32 +0300

    create user api fix

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose');
 const { User } = require('../models/user.model');
 
-module.exports.getAllUsers = async (req, res) => {
+const getAllUsers = async (req, res) => {
     let users = await User.find({});
     return res.send(users);
 };
 
-module.exports.getUser = async (req, res) => {
+const getUser = async (req, res) => {
     let userId = req.params.id;
     if (!mongoose.Types.ObjectId.isValid(userId))
         return res.status(400).send('Invalid object id');
@@ -16,11 +16,11 @@ module.exports.getUser = async (req, res) => {
     return res.send(user);
 };
 
-module.exports.createUser = async (req, res) => {
+const createUser = async (req, res) => {
     console.log(req.body);
     let user = new User({
-        user: req.body, name,
-        email = req.body.email,
+        name: req.body.name,
+        email: req.body.email,
         gender: req.body.gender
     });
 
@@ -28,7 +28,7 @@ module.exports.createUser = async (req, res) => {
     return res.send(user);
 };
 
-module.exports.updateUser = async (req, res) => {
+const updateUser = async (req, res) => {
     let userId = req.params.id;
     User.findOneAndUpdate(userId, req.body, { new: true })
         .then(user => {
@@ -39,8 +39,10 @@ module.exports.updateUser = async (req, res) => {
         });
 };
 
-module.exports.deleteUser = async (req, res) => {
+const deleteUser = async (req, res) => {
     let userId = req.params.id;
     await User.findByIdAndDelete(userId);
     return res.send('User deleted');
 };
+
+module.exports = { deleteUser, updateUser, createUser, getAllUsers, getUser }
