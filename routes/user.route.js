5d6b924c75ba8a9af693cf8fc4b3a2a51d618068
Carol-Barno <cbarno@innovexsolutions.co.ke>
2022-01-20 14:09:19 +0300

    linting module

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,16 +1,18 @@
+/* eslint-disable linebreak-style */
 const express = require('express');
+
 const router = express.Router();
 const controller = require('../controllers/user.controller');
 
 router
-    .route('/')
-    .get(controller.getAllUsers)
-    .post(controller.createUser);
+  .route('/')
+  .get(controller.getAllUsers)
+  .post(controller.createUser);
 
 router
-    .route('/id:')
-    .get(controller.getUser)
-    .put(controller.updateUser)
-    .delete(controller.deleteUser);
+  .route('/id:')
+  .get(controller.getUser)
+  .put(controller.updateUser)
+  .delete(controller.deleteUser);
 
 module.exports = router;
